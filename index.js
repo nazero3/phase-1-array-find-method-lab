b3db930166bf73791e96db698bd2938bbc38a152
nazero3 <nazerrabah1994@gmail.com>
2023-03-21 15:37:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(arr) {
+
+    const year = arr.find(element => element.result === 'W');
+    if (year === undefined)
+        return undefined;
+    else if (year.result === 'W' && year.result !== undefined)
+        return year.year;
+
+    
+        
+}
